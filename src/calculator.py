e27787bb33c2613545947f447f7408c41e787223
konstmalinin-maker <konstmalinin@gmail.com>
2026-09-23 16:34:21 +0300

    feat(calculator): add module skeleton with add and subtract stubs

---
+++ src/calculator.py
@@ -0,0 +1,11 @@
+"""Модуль арифметических операций."""
+
+
+def add(a: float, b: float) -> float:
+    """Возвращает сумму двух чисел."""
+    raise NotImplementedError
+
+
+def subtract(a: float, b: float) -> float:
+    """Возвращает разность двух чисел."""
+    raise NotImplementedError
